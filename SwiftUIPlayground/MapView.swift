b4f1f7426cf81dd98ee4ee01e6d9268779ca317e
Alex Matuzenko <omatuzenko@gmail.com>
2019-06-04 15:21:44 +0300

    Added map view

---
+++ SwiftUIPlayground/MapView.swift
@@ -0,0 +1,34 @@
+//
+//  MKMapView.swift
+//  SwiftUIPlayground
+//
+//  Created by Alex Matuzenko on 04/06/2019.
+//  Copyright © 2019 Alex Matuzenko. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+// The UIViewRepresentable protocol has two requirements you need to add:
+// a makeUIView(context:) method that creates an MKMapView, and
+// an updateUIView(_:context:) method that configures the view and responds to any changes.
+struct MapView : UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
